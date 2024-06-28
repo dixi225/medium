7b9e37d3a41caa05b3c86ce7bda8c7da244088ea
Harsh Dixit <harshdixit573@gmail.com>
2024-06-28 06:44:15 -0400

    Signin controller added

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -3,11 +3,12 @@ import { withAccelerate } from "@prisma/extension-accelerate"
 import { Context } from "hono"
 import jwt from 'jsonwebtoken'
 
-
 export const signUpController=async (c:Context)=>{
-    const prisma=new PrismaClient().$extends(withAccelerate())
+    const prisma=new PrismaClient({
+    datasourceUrl:c.env.DATABASE_URL
+    }   
+    ).$extends(withAccelerate())
     const body=await c.req.json()
-    c.env
     const user=await prisma.user.create({
      data:{
         email: body.email,
@@ -17,12 +18,38 @@ export const signUpController=async (c:Context)=>{
         posts:body.posts
      }   
     })
+
     const{id}=user
     const scrt=c.env.JWT_SECRET
     const token=jwt.sign(id,scrt)
     return c.json({
         message:"User Created",
         token:token})
-    
 }
 
+export const signInController=async(c:Context)=>{
+    const prisma=new PrismaClient({
+        datasourceUrl:c.env.DATABASE_URL
+        }   
+        ).$extends(withAccelerate())
+    const{email,password}=await c.req.json()    
+    const user=await prisma.user.findFirst({
+        where:{
+            email:email
+        },
+        select:{
+            id:true,
+            password:true
+        }
+    })    
+
+    if(!user) return c.json({message:"user not found"})
+    if(user.password!=password) return c.json({message:"Invalid Password"})
+        
+    const token=jwt.sign(user.id,c.env.JWT_SECRET)
+
+    return c.json({
+        message:"User Valid",
+        Token:token
+    })
+}
--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,10 +1,10 @@
 import {  Hono } from "hono";
-import { signUpController } from "../controllers/user.controller";
+import { signInController, signUpController } from "../controllers/user.controller";
 
 
 const user=new Hono()
 
-user.post('/signin',(c)=>{return c.text("Request Recived")})
+user.post('/signin',signInController)
 user.post('/signup',signUpController)
 
 export default user
